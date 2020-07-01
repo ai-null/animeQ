5dee65cf609bacc8f13d21ffc779a371f01725c2
MAinulYaqin <ainul.y9b@gmail.com>
2020-07-01 17:37:49 +0700

    move adapter to different file and package

---
+++ app/src/main/java/com/gabutproject/animeq/adapter/SeasonalAnimeAdapter.kt
@@ -0,0 +1,45 @@
+package com.gabutproject.animeq.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.gabutproject.animeq.R
+import com.gabutproject.animeq.network.AnimeProperty
+
+class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val title = view.findViewById<TextView>(R.id.item_title)
+
+    companion object {
+        @LayoutRes
+        val LAYOUT = R.layout.seasonal_anime_item
+    }
+}
+
+class SeasonalAnimeAdapter : RecyclerView.Adapter<ItemViewHolder>() {
+
+    var data = listOf<AnimeProperty>()
+        set(value) {
+            field = value
+
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(ItemViewHolder.LAYOUT, parent, false)
+
+        return ItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = data[position]
+
+        holder.title.text = item.title
+    }
+
+}
--- app/src/main/java/com/gabutproject/animeq/ui/MainActivity.kt
+++ app/src/main/java/com/gabutproject/animeq/ui/MainActivity.kt
@@ -2,24 +2,19 @@ package com.gabutproject.animeq.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
 import com.gabutproject.animeq.R
+import com.gabutproject.animeq.adapter.SeasonalAnimeAdapter
 import com.gabutproject.animeq.databinding.ActivityMainBinding
-import com.gabutproject.animeq.network.AnimeProperty
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainActivityViewModel()
 
-    private val adapter: SeasonalAnimeAdapter = SeasonalAnimeAdapter()
+    private val adapter = SeasonalAnimeAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,56 +22,21 @@ class MainActivity : AppCompatActivity() {
         // set the activity to use the layout
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val viewModel = MainActivityViewModel()
-
         // define viewModel and lifecycleOwner to observe changed data
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
+        binding.seasonalAnimeList.layoutManager = GridLayoutManager(this, 2)
         binding.seasonalAnimeList.adapter = adapter
+
+        updateLiveData()
+    }
+
+    private fun updateLiveData() {
         viewModel.seasonalAnime.observe(this, Observer {
             it?.let {
                 adapter.data = it.anime
-                Log.i("anime data", "${adapter.data[0]}")
-                Log.i("anime list", "${it.anime[0]}")
             }
         })
     }
-}
-
-class SeasonalAnimeViewHolder(item: View) : RecyclerView.ViewHolder(item) {
-    val title: TextView = item.findViewById(R.id.item_title)
-
-    companion object {
-        @LayoutRes
-        val LAYOUT = R.layout.seasonal_anime_item
-    }
-}
-
-class SeasonalAnimeAdapter : RecyclerView.Adapter<SeasonalAnimeViewHolder>() {
-    var data = listOf<AnimeProperty>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeasonalAnimeViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val withDataBinding = layoutInflater.inflate(
-            SeasonalAnimeViewHolder.LAYOUT,
-            parent,
-            false
-        )
-
-        return SeasonalAnimeViewHolder(withDataBinding)
-    }
-
-    override fun getItemCount(): Int = data.size
-
-    override fun onBindViewHolder(holder: SeasonalAnimeViewHolder, position: Int) {
-        val item = data[position]
-        holder.title.text = item.title
-        Log.i("anime title", item.title)
-    }
-
 }
