6a4b82d40347c92da60fea9d2251cb1c760ebe40
MAinulYaqin <ainul.y9b@gmail.com>
2020-07-10 04:48:27 +0700

    add some style to detail activity

--- app/src/main/java/com/gabutproject/animeq/ui/DetailActivity.kt
+++ app/src/main/java/com/gabutproject/animeq/ui/DetailActivity.kt
@@ -1,31 +1,37 @@
 package com.gabutproject.animeq.ui
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.gabutproject.animeq.R
+import com.gabutproject.animeq.databinding.DetailActivityBinding
 import com.gabutproject.animeq.viewmodel.DetailViewModel
 
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var viewModel: DetailViewModel
 
+    private lateinit var binding: DetailActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.detail_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.detail_activity)
+        binding.lifecycleOwner = this
 
-        val headline: TextView = this.findViewById(R.id.headline)
 
         if (intent.hasExtra("mal_id")) {
             val data: Int = intent.extras!!.getInt("mal_id")
 
             viewModel = DetailViewModel(data)
+            updateLiveData()
         }
+    }
 
+    private fun updateLiveData() {
         viewModel.animeProperty.observe(this, Observer {
             it?.let {
-                headline.text = it.title
+                binding.property = it
             }
         })
     }
