9a5ee540b8d319b5120ae172d899d99a2df733f3
MAinulYaqin <ainul.y9b@gmail.com>
2020-07-16 05:10:49 +0700

    add comments doc to BindingAdapter

--- app/src/main/java/com/gabutproject/animeq/util/BindingAdapter.kt
+++ app/src/main/java/com/gabutproject/animeq/util/BindingAdapter.kt
@@ -9,6 +9,9 @@ import com.gabutproject.animeq.R
 
 /**
  * Image processing using glide
+ *
+ * @param imageUrl String?
+ *
  * TODO: implement broken image & loading image
  */
 @BindingAdapter("imageUrl")
@@ -24,6 +27,9 @@ fun ImageView.imageUrl(imageUrl: String?) {
 
 /**
  * used for card with date or ui that implement date
+ *
+ * @param date String?
+ *
  * if the anime date was confirmed, return actual date
  * if not, return TBA or To Be Announced
  */
@@ -32,6 +38,16 @@ fun TextView.startDate(date: String?) {
     text = date ?: context.getString(R.string.to_be_announced)
 }
 
+/**
+ * used for detail activity.
+ *
+ * @param status Boolean
+ *  airing status
+ *
+ * the data fetched from server returns Boolean value.
+ * so in order to make readable information, this will change the value to:
+ * Aired == true / TBA
+ */
 @BindingAdapter("status")
 fun TextView.status(status: Boolean) {
     text = if (status) {
